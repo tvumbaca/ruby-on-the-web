cb76b5bcd4a6ed57e2d6d49e8b4a633498163904
Cornelius Soon <cornelius.soon@gmail.com>
2016-08-09 09:39:26 +1200

    Add receive user input

--- ruby-server/browser.rb
+++ ruby-server/browser.rb
@@ -2,9 +2,19 @@ require 'socket'
 
 host = 'localhost'
 port = 2000
-path = "/index.html"
 
-request = "GET #{path} HTTP/1.1\r\n\r\n"
+input = gets.chomp
+message = input.split(/\s+/)
+rest_verb = message[0]
+url_path = message[1]
+browser_http_version = message[2]
+
+path = "index.html"
+
+request = "#{rest_verb} #{url_path} #{browser_http_version}\r\n\r\n"
+
+#request = "GET #{path} HTTP/1.1\r\n\r\n"
+
 
 socket = TCPSocket.open(host, port)
 socket.print(request)
@@ -24,20 +34,8 @@ socket.close
 
 
 
+#* modify client to accept input for sending requests - GET and POST
+#* set up header request line
+#* in POST, ask for username and email
+#
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-puts
